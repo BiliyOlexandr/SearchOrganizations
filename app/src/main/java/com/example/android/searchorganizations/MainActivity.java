f8c6ee59cb3d6d7ff5cdd27d3bab4efcf8441355
Biliy Olexandr <biliyoleksandrwork@gmail.com>
2017-09-22 18:28:23 +0300

    Add EditText

--- app/src/main/java/com/example/android/searchorganizations/MainActivity.java
+++ app/src/main/java/com/example/android/searchorganizations/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.android.searchorganizations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.TextKeyListener;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-  }
-}
+    EditText myEditText = (EditText) findViewById(R.id.search_edit_text);
 
-  //getSupportActionBar().hide();
+  }
+
+
+}
   //getSupportActionBar().show();
+//getSupportActionBar().hide();
