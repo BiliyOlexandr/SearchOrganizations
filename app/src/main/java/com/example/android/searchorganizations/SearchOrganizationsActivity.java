8fd5076af8cc8b40c42dbbe5ded82a065f7a89e2
Biliy Olexandr <biliyoleksandrwork@gmail.com>
2017-09-23 20:14:40 +0300

    Hide toolbar from click to edit text and title enable has false

--- app/src/main/java/com/example/android/searchorganizations/SearchOrganizationsActivity.java
+++ app/src/main/java/com/example/android/searchorganizations/SearchOrganizationsActivity.java
@@ -2,17 +2,20 @@ package com.example.android.searchorganizations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
 
 public class SearchOrganizationsActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_search_organizations);
-    EditText myEditText = (EditText) findViewById(R.id.search_edit_text);
+    getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-
+    findViewById(R.id.search_edit_text).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        getSupportActionBar().hide();
+      }
+    });
   }
 
 }
-  //getSupportActionBar().show();
