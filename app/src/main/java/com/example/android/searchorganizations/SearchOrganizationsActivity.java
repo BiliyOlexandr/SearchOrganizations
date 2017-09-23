186b30a8705e59ecb533224e1f40217e74b4728b
Biliy Olexandr <biliyoleksandrwork@gmail.com>
2017-09-23 19:18:57 +0300

    Add classes User and Repositiry

--- app/src/main/java/com/example/android/searchorganizations/SearchOrganizationsActivity.java
+++ app/src/main/java/com/example/android/searchorganizations/SearchOrganizationsActivity.java
@@ -2,14 +2,13 @@ package com.example.android.searchorganizations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchOrganizationsActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_search_organizations);
     EditText myEditText = (EditText) findViewById(R.id.search_edit_text);
 
 
